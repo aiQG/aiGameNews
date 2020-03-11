80b61ac517ef4f05e7ee4c4fe4451098f30a9250
aiQG_ <lovequange1314@vip.qq.com>
2020-03-11 20:24:06 +0800

    Initial Commit

---
+++ TheGameNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheGameNews
+//
+//  Created by 周测 on 3/11/20.
+//  Copyright © 2020 aiQG_. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
