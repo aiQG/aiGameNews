68748e5785682d574f5ad5c5ee1aab77ebe90cd7
aiQG_ <lovequange1314@vip.qq.com>
2020-03-13 15:45:28 +0800

    CrawlerResult struct

--- TheGameNews/WebCrawler.swift
+++ TheGameNews/WebCrawler.swift
@@ -9,23 +9,48 @@
 //取得基本信息
 import Foundation
 //import Kanna
+
+struct CrawlerResult: Identifiable {
+	var id: Int = -1
+	var link: String = "https://aiqg.vip"
+	var imageLink: String = "https://raw.githubusercontent.com/aiQG/aiQG.github.io/master/assets/images/blacktocat.png"
+	var title: String = "NULL"
+	var content: String = "null"
+	var date: String = "***"
+}
+
+
 class WebCrawler: ObservableObject{
-	let url = URL(string: "https://www.wowchina.com/zh-cn/news.frag?page=1")!
-	@Published var dic: [(String?, String?, String?, String?, String?)] = []
+	var url: URL
+	// (ID, Link, Image, Title, Content, Date)
+	@Published var result: [CrawlerResult] {
+		didSet {
+			print("aaa")
+		}
+	}
+	
+	init(url: URL = URL(string: "https://www.wowchina.com/zh-cn/news.frag?page=1")!) {
+		self.url = url
+		self.result = []
+		self.call()
+	}
 	
 	func call() {
 		let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
 			guard let html = data else { return }
 			if let doc = try? HTML(html: html, encoding: .utf8) {
 				var Link: [String?] = []
-
+				
 				for link in doc.xpath(#"//a[@class="Link NewsBlog-link"]"#) {
 					Link += [link["href"] ?? nil]
 				}
 				
 				var imageLink: [String?] = []
 				for image in doc.xpath(#"//div[@class="Tile-bg"]"#) {
-					imageLink += [image["style"] ?? nil]
+					let str = image["style"] ?? ""
+					let temp = str[str.range(of: "\"//[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]\"", options: .regularExpression)!]
+					
+					imageLink += ["http:" + temp.replacingOccurrences(of: "\"", with: "")]
 				}
 				
 				var title: [String?] = []
@@ -44,11 +69,17 @@ class WebCrawler: ObservableObject{
 				}
 				// finally get tuples
 				for i in 0..<max(Link.count, imageLink.count, title.count, content.count, date.count) {
-					self.dic.append((Link[i], imageLink[i], title[i], content[i], date[i]))
+					self.result.append(
+						CrawlerResult(id: i, link: Link[i] ?? "https://aiqg.vip",
+									  imageLink: imageLink[i] ?? "https://raw.githubusercontent.com/aiQG/aiQG.github.io/master/assets/images/blacktocat.png",
+									  title: title[i] ?? "NULL",
+									  content: content[i] ?? "null",
+									  date: date[i] ?? "**"))
 				}
 			}
 		}
 		task.resume()
-		
+		sleep(10)
+		print("endd")
 	}
 }
