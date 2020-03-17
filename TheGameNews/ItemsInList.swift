e48f9f0283cf61d87230dd357f01ba89df7940ee
aiQG_ <lovequange1314@vip.qq.com>
2020-03-17 10:25:07 +0800

    换页按钮

--- TheGameNews/ItemsInList.swift
+++ TheGameNews/ItemsInList.swift
@@ -9,17 +9,37 @@
 import SwiftUI
 
 struct ItemsInList: View {
-	@ObservedObject private var crawlerData = WebCrawler()
+	@State private var page = 1
+	@ObservedObject private var crawlerData = WebCrawler() // 放入要爬的页数
 	
 	
 	var body: some View {
-		NavigationView {
-			List(crawlerData.result ?? [CrawlerResult()], id: \.id) { i in
-				NavigationLink(destination: WebView(request: i)){
-					ListItem(URLData: i)
+		VStack {
+			NavigationView {
+				List(crawlerData.result ?? [CrawlerResult()], id: \.id) { i in
+					NavigationLink(destination: WebView(request: i)){
+						ListItem(URLData: i)
+					}
+				}
+				.navigationBarTitle(Text("WoW"))
+			}
+			HStack {
+				Button(action: {
+					self.page = self.page > 0 ? self.page - 1 : 1
+				}) {
+					Text("     <      ")
+						.foregroundColor(.secondary)
+						.font(.title)
+				}
+				Spacer()
+				Button(action: {
+					self.page = self.page < 100 ? self.page + 1 : 100
+				}) {
+					Text("      >     ")
+						.foregroundColor(.secondary)
+						.font(.title)
 				}
 			}
-		.navigationBarTitle(Text("WoW"))
 		}
 	}
 }
@@ -27,5 +47,6 @@ struct ItemsInList: View {
 struct ItemsInList_Previews: PreviewProvider {
 	static var previews: some View {
 		ItemsInList()
+			//.environment(\.colorScheme, .dark)
 	}
 }
