1867fe8a406d968f449705635f8fd567cdb7a6b9
aiQG_ <lovequange1314@vip.qq.com>
2020-03-20 12:21:08 +0800

    爬虫整合数据异常处理

--- TheGameNews/WoWWebCrawler.swift
+++ TheGameNews/WoWWebCrawler.swift
@@ -78,11 +78,11 @@ class WoWWebCrawler: ObservableObject{
 					
 					for i in 0..<max(Link.count, imageLink.count, title.count, content.count, date.count) {
 						output!.append(
-							CrawlerResult(id: i, link: Link.count > i ? Link[i]! : "https://aiqg.vip",
-										  imageLink: imageLink.count > i ? imageLink[i]! : "https://raw.githubusercontent.com/aiQG/aiQG.github.io/master/assets/images/blacktocat.png",
-										  title: title[i] ?? "NULL",
-										  content: content[i] ?? "null",
-										  date: date[i] ?? "**"))
+						CrawlerResult(id: i, link: Link.count > i ? Link[i]! : "https://aiqg.vip",
+									  imageLink: imageLink.count > i ? imageLink[i]! : "https://raw.githubusercontent.com/aiQG/aiQG.github.io/master/assets/images/blacktocat.png",
+									  title: title.count > i ? title[i]! : "NULL",
+									  content: content.count > i ? content[i]! : "null",
+									  date: date.count > i ? date[i]! : "**"))
 					}
 				}
 				return output
